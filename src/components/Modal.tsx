d159a4c37d0b88d09084a5cf268592295e0b231e
Marcelin <marceloaugusto10123@gmail.com>
2022-03-22 17:32:38 -0300

    update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Select, useDisclosure, useToast } from "@chakra-ui/react"
 import { ShoppingCartOutlined } from "@ant-design/icons"
 
 export const ModalCart = () => {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
-
+    const [nome, setNome] = useState('')
+    const [produtos, setProdutos] = useState('')
     const initialRef = React.useRef()
     const finalRef = React.useRef()
     const toast = useToast()
 
+
+    const data = {
+        nome,
+        produtos
+
+    }
+    function SavePedido() {
+        toast({
+            title: 'Pedido efetuado!',
+            description: ` ${nome}, Seu pedido foi efetuado com sucesso!`,
+            status: 'success',
+            duration: 3000,
+            isClosable: true,
+        })
+        console.log(data)
+    }
 
     return (
         <>
@@ -23,28 +40,25 @@ export const ModalCart = () => {
                 <ModalContent>
                     <ModalHeader>Escolha seus produtos:</ModalHeader>
                     <ModalCloseButton _focus={{}} />
+                    <FormControl>
+
+                    </FormControl>
                     <ModalBody pb={6}>
-                        <Select>
+                        <FormControl paddingY={3}>
+
+                            <Input onChange={(e => setNome(e.target.value))} placeholder='Nome completo' />
+                        </FormControl>
+                        <Select onChange={(e => setProdutos(e.target.value))} >
                             <option>Produtos</option>
                             <option>PDV</option>
                             <option>NFE</option>
                             <option>Comanda</option>
-
-
                         </Select>
-
-
                     </ModalBody>
-
                     <ModalFooter>
                         <Button _focus={{}} onClick={() =>
-                            toast({
-                                title: 'Pedido efetuado!',
-                                description: "Seu pedido foi efetuado com sucesso!",
-                                status: 'success',
-                                duration: 3000,
-                                isClosable: true,
-                            })
+                            SavePedido()
+
                         }
                             colorScheme='blue' mr={3}>
                             Salvar
